40d9414e6bbf0d0606f10f2ccab1f27bfd324cea
omar-home-dekstop <medzhidov.omar11@gmail.com>
2023-10-25 11:43:50 +0300

    ИМПОРТИРОВАЛ стили из папки swiper/modules и прочее

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,28 @@
-import Swiper, { Pagination } from "swiper";
+import Swiper from "swiper";
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+
+import { Pagination } from "swiper/modules";
+
+// Styles must use direct files imports
+import "swiper/swiper.scss"; // core Swiper
+import "swiper/modules/navigation.scss"; // Navigation module
+import "swiper/modules/pagination.scss"; // Pagination module
 
 export const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
   slidesPerView: "auto",
   spaceBetween: 5,
-  modules: [Pagination],
   breakpoints: {
     768: {
       enabled: false,
     },
   },
-
+  modules: [Pagination],
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    // type: "bullets",
+    type: "bullets",
   },
 });
+console.log("swiper");
