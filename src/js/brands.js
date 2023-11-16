0dbc52e3fb47c9a90f3d149455e864edff4048e0
SchoolBoy98 <medzhidov.omar11@gmail.com>
2023-11-16 12:07:20 +0300

    classlist

--- src/js/brands.js
+++ src/js/brands.js
@@ -4,29 +4,22 @@ const brandBtnImg = brands.querySelector(".btn-wrapper__expand");
 const items = Array.from(brands.querySelectorAll(".brands__item"));
 
 for (let i = 0; i < items.length; i++) {
-  items[i].classList.add(`hide-desktop-items`);
-  items[i].classList.add(`hide-tablet-items`);
+  items[i].classList.add(`hide-desktop-items`, `hide-tablet-items`);
 }
 
 brandBtn.addEventListener("click", () => {
   brandBtn.classList.toggle("content-hidden");
   if (brandBtn.classList.contains("content-hidden")) {
     for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove(`show-tablet-items`);
-      items[i].classList.remove(`show-desktop-items`);
-
-      items[i].classList.add(`hide-tablet-items`);
-      items[i].classList.add(`hide-desktop-items`);
+      items[i].classList.remove(`show-tablet-items`, `show-desktop-items`);
+      items[i].classList.add(`hide-tablet-items`, `hide-desktop-items`);
     }
     brandBtnImg.classList.remove(`rotate`);
     brandBtn.textContent = "Показать все";
   } else {
     for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove(`hide-tablet-items`);
-      items[i].classList.remove(`hide-desktop-items`);
-
-      items[i].classList.add(`show-tablet-items`);
-      items[i].classList.add(`show-desktop-items`);
+      items[i].classList.remove(`hide-tablet-items`, `hide-desktop-items`);
+      items[i].classList.add(`show-tablet-items`, `show-desktop-items`);
     }
     brandBtnImg.classList.add(`rotate`);
     brandBtn.textContent = "Скрыть";
--- src/js/repair.js
+++ src/js/repair.js
@@ -4,29 +4,22 @@ const buttonImg = repair.querySelector(".btn-wrapper__expand");
 const items = Array.from(repair.querySelectorAll(".repair__item"));
 
 for (let i = 0; i < items.length; i++) {
-  items[i].classList.add(`hide-desktop-items`);
-  items[i].classList.add(`hide-tablet-items`);
+  items[i].classList.add(`hide-desktop-items`, `hide-tablet-items`);
 }
 
 button.addEventListener("click", () => {
   button.classList.toggle("content-hidden");
   if (button.classList.contains("content-hidden")) {
     for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove(`show-tablet-items`);
-      items[i].classList.remove(`show-desktop-items`);
-
-      items[i].classList.add(`hide-tablet-items`);
-      items[i].classList.add(`hide-desktop-items`);
+      items[i].classList.remove(`show-tablet-items`, `show-desktop-items`);
+      items[i].classList.add(`hide-tablet-items`, `hide-desktop-items`);
     }
     buttonImg.classList.remove(`rotate`);
     button.textContent = "Показать все";
   } else {
     for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove(`hide-tablet-items`);
-      items[i].classList.remove(`hide-desktop-items`);
-
-      items[i].classList.add(`show-tablet-items`);
-      items[i].classList.add(`show-desktop-items`);
+      items[i].classList.remove(`hide-tablet-items`, `hide-desktop-items`);
+      items[i].classList.add(`show-tablet-items`, `show-desktop-items`);
     }
     buttonImg.classList.add(`rotate`);
     button.textContent = "Скрыть";
